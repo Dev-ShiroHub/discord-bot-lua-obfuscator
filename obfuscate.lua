f3f8be262901d64cdcc693edf0fc20929cdcaac2
Dev-ShiroHub <akunffpribadi214@gmail.com>
2026-04-12 21:42:58 +0700

    Update obfuscate.lua

--- obfuscate.lua
+++ obfuscate.lua
@@ -2,11 +2,11 @@ local thing = [[
 --SCRIPT
 ]]
 
-local jcm="\n\n --// Obfuscated with nObf by Nertigel \n\n"
+local jcm="\n\n --// ts file ws protected from \nCitizenfuscate | by TenshiDev \n\n"
 local jcfs={"ilInErTiGElilI","ilINertIgeL5391nErt","IlinErTiGeLilI","iElIinlTI5391ilI","nErtIgElwAshErE","nE5rtIgElwAshErE","nErtIgEl5391E","nE5391rtIgEl5391E"}
 local jcq="local "..jcfs[math.random(1,#jcfs)].." = (5*3-2/8+9*2/9+8*3)".." "
-local jcn1="function nErTiGeL_ilIilI("..jcfs[math.random(1,#jcfs)]..")"..jcq.."end".." "
-local jcn2="function nErtIgEL_ilIilI("..jcfs[math.random(1,#jcfs)]..")"..jcq.."end".." "
+local jcn1="return(function(...)local nErTiGeL_ilIilI("..jcfs[math.random(1,#jcfs)]..")"..jcq.."end".." "
+local jcn2="return(function(...)local nErtIgEL_ilIilI("..jcfs[math.random(1,#jcfs)]..")"..jcq.."end".." "
 local junkcode=jcm..jcn1..jcn2..jcq
 
 local encoded = thing:gsub(".", function(bb) return "\\" .. bb:byte() end) or thing .. "\""
